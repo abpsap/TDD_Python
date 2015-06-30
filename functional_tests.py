20004e6ef03104ca9c5457b0453b4c3cb92220c3
Anand Patil <abpsap@gmail.com>
2015-06-29 20:41:19 -0400

    added unittest feature

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitortest(unittest.testCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has head about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        assert 'To-Do' in self.browser.title
+        self.fail()
+
+        # She is invited to enter a to-do item straight away
+        # She types "But peacock feathers" into a text box
+        # When she hits enter , the page updates and now the page lists
+        # "1: Buy peacock feathers' as an item in a to-do list
+        # There is still a text box inviting her to add another item
+        # The page updates again and now shows both items on her list
+        # Edith navigates to another page and comes back to see if her to-do still exists and it does
+        # She logs out.
+
+
+if (__name__) == "__main__":
+    unittest.main(warnings='ignore')
+
