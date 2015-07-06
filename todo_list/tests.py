1c57672c7a097cc83108816d83bc6c2bda94206a
Anand Patil <abpsap@gmail.com>
2015-07-05 23:19:20 -0400

    Redirect after POST, and show all items in template

--- todo_list/tests.py
+++ todo_list/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from todo_list.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from todo_list.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -23,6 +24,11 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertTrue(expected_html, response.content.strip().decode())
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertTrue(Item.objects.count(), 0)
+
     def test_home_page_can_save_a_POST_request(self):
         try:
             request = HttpRequest()
@@ -31,10 +37,41 @@ class HomePageTest(TestCase):
 
         request.method = 'POST'
         request.POST['item_text'] = 'A new user profile'
-        response = home_page(request)
-        self.assertTrue('A new user profile', response.content.strip().decode())
 
-        expected_html = render_to_string(
-            'home.html',
-        {'new_item_text': 'A new user profile'}
-        )
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new user profile')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
+
+class ItemModelTest (TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First Profile item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'List Profile second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0];
+        second_saved_item = saved_items[1];
+        self.assertEqual(first_saved_item.text, 'First Profile item')
+        self.assertEqual(second_saved_item.text, 'List Profile second')
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from todo_list.models import Item
 
 # Create your views here.
 def home_page(request):
 #    return HttpResponse('<html><title>Match Making Website</title></html>')
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+#    return render(request, 'home.html', {
+#        'new_item_text': request.POST.get('item_text', ''),
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items':items})
+
 
